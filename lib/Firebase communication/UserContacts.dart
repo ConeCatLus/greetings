81f0576340646641eeb742317b43953344232eeb
Gustav Carlstedt <gustav.carlstedt@live.se>
2021-03-14 12:50:31 +0100

    Fixade med databas koppling

--- lib/Firebase communication/UserContacts.dart
+++ lib/Firebase communication/UserContacts.dart
@@ -1,27 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Contacts {
+class Contact {
   // 1
   String user;
   // 2
   DocumentReference reference;
   // 3
-  Contacts(this.user);
+  Contact(this.user);
   // 4
-  factory Contacts.fromJson(Map<dynamic, dynamic> json) => _contactsfromjson(json);
+  factory Contact.fromJson(Map<dynamic, dynamic> json) => _contactsfromjson(json);
   // 5
   Map<String, dynamic> toJson() => _contactstojson(this);
   @override
-  String toString() => "Contacts<$user>";
+  String toString() => "Contact<$user>";
 }
 
-Contacts _contactsfromjson(Map<dynamic, dynamic> json) {
-  return Contacts(
+Contact _contactsfromjson(Map<dynamic, dynamic> json) {
+  return Contact(
     json['user'] as String,
   );
 }
 //2
-Map<String, dynamic> _contactstojson(Contacts instance) =>
+Map<String, dynamic> _contactstojson(Contact instance) =>
     <String, dynamic> {
       'user': instance.user,
     };
--- lib/Firebase communication/UserData.dart
+++ lib/Firebase communication/UserData.dart
@@ -5,44 +5,59 @@ class UserData {
   String firstname;
   String lastname;
 
-  List<Contacts> vaccinations = List<Contacts>();
+  // ignore: deprecated_member_use
+  List<Contact> contacts = List<Contact>();
 
   DocumentReference reference;
   
-  UserData(this.firstname, this.lastname);
+  UserData(this.firstname, this.lastname, {this.contacts});
   // 5
   factory UserData.fromSnapshot(DocumentSnapshot snapshot) {
-    UserData newPet = UserData.fromJson(snapshot.data);
-    newPet.reference = snapshot.reference;
-    return newPet;
+    UserData newUser = UserData.fromJson(snapshot.data());
+    newUser.reference = snapshot.reference;
+    return newUser;
   }
   // 6
-  factory UserData.fromJson(Map<String, dynamic> json) => _petfromjson(json);
+  factory UserData.fromJson(Map<String, dynamic> json) => _userfromjson(json);
   // 7
-  Map<String, dynamic> toJson() => _pettojson(this);
+  Map<String, dynamic> toJson() => _usertojson(this);
   @override
   String toString() => "UserData<$firstname>";
 }
 
-UserData _petfromjson(Map<String, dynamic> json) {
+UserData _userfromjson(Map<String, dynamic> json) {
   return UserData(
     json['firstname'] as String,
     json['lastname'] as String,
+    contacts: _convertContacts(json['contacts'] as List)
   );
 }
 
-Map<String, dynamic> _pettojson(UserData instance) => <String, dynamic> {
-      'firstname': instance.firstname,
-      'lastname': instance.lastname,
-    };
-
-List<Map<String, dynamic>> _contactslist(List<Contacts> vaccinations) {
-  if ( == null) {
+List<Contact> _convertContacts(List contactList) {
+  if (contactList == null) {
     return null;
   }
-  List<Map<String, dynamic>> contactsMap =List<Map<String, dynamic>>();
-  vaccinations.forEach((vaccination) {
-    vaccinationMap.add(vaccination.toJson());
+  // ignore: deprecated_member_use
+  List<Contact> contacts =  List<Contact>();
+    contactList.forEach((contact) {
+      contacts.add(Contact.fromJson(contact));
   });
-  return vaccinationMap;
+  return contacts;
+}
+
+Map<String, dynamic> _usertojson(UserData instance) => <String, dynamic> {
+      'firstname': instance.firstname,
+      'lastname': instance.lastname,
+      'contacts': _ContactList(instance.contacts),
+    };
+// 4
+List<Map<String, dynamic>> _ContactList(List<Contact> contacts) {
+  if (contacts == null) {
+    return null;
+  }
+  List<Map<String, dynamic>> contactMap =List<Map<String, dynamic>>();
+  contacts.forEach((vaccination) {
+    contactMap.add(vaccination.toJson());
+  });
+  return contactMap;
 }
