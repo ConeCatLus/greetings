c46667c18f8717e1aaecd17f00cd4b4507f517d3
Asma Syed <asma.syed@outlook.com>
2021-04-27 21:50:55 +0200

    pillillilli

--- lib/pages/ContactViewPage.dart
+++ lib/pages/ContactViewPage.dart
@@ -6,16 +6,34 @@ class ContactViewPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        title: Text("Asma Syed"),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+      body: Center( child: 
+          Padding(
+            child: Row(children: [Column(children: [actionButton(), actionButton()]), Column(children: [actionButton(), actionButton()] ) ]),  // Add Contacts to see in our page
+            padding: EdgeInsets.all(5.0),  // Add padding between contacts
         ),
       ),
     );
   }
 }
+
+Widget actionButton() { 
+  return Container(
+    height: 180, 
+    width: 90,                                          // Contact Container height
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(10),              // Rouded edges to Contact Container
+        boxShadow: [                                          // Contact Container shadowing
+          BoxShadow(
+            color: Colors.blueGrey.withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 3,
+            offset: Offset(0, 3),                             // changes position of shadow
+          ),
+        ],       
+      ),
+  );
+}
+  
